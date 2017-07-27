7a5ff1c6662368f0d49438f73adedb7da2d64278
RJ DeCramer <decramrj@rose-hulman.edu>
2017-07-27 16:21:33 -0400

    Fixed bug where robot's screen was different on startup.

--- 2D Simulation/index.js
+++ 2D Simulation/index.js
@@ -424,8 +424,6 @@ function instructiontime() {
 }
 
 function setUpInitialPosition() {
-    p_left = [];
-    p_top = [];
     for (var i = 0; i < NumBlocks; i++) {
         var tLeft = Math.floor(Math.random()*random_x) * 50 + init_x,
         tTop  = Math.floor(Math.random()*random_y) * 50 + init_y;
